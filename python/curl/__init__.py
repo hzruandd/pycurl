4aa754f00dd58c6741edc23b52255f6e506ed705
hzruandd <82433422@qq.com>
2015-12-25 14:22:45 +0800

    optimized code

--- python/curl/__init__.py
+++ python/curl/__init__.py
@@ -114,39 +114,26 @@ class Curl:
 
     def info(self):
         "Return a dictionary with all info on the last response."
+        #NOTE(hzruandd):optimized code here.
         m = {}
-        m['effective-url'] = self.handle.getinfo(pycurl.EFFECTIVE_URL)
-        m['http-code'] = self.handle.getinfo(pycurl.HTTP_CODE)
-        m['total-time'] = self.handle.getinfo(pycurl.TOTAL_TIME)
-        m['namelookup-time'] = self.handle.getinfo(pycurl.NAMELOOKUP_TIME)
-        m['connect-time'] = self.handle.getinfo(pycurl.CONNECT_TIME)
-        m['pretransfer-time'] = self.handle.getinfo(pycurl.PRETRANSFER_TIME)
-        m['redirect-time'] = self.handle.getinfo(pycurl.REDIRECT_TIME)
-        m['redirect-count'] = self.handle.getinfo(pycurl.REDIRECT_COUNT)
-        m['size-upload'] = self.handle.getinfo(pycurl.SIZE_UPLOAD)
-        m['size-download'] = self.handle.getinfo(pycurl.SIZE_DOWNLOAD)
-        m['speed-upload'] = self.handle.getinfo(pycurl.SPEED_UPLOAD)
-        m['header-size'] = self.handle.getinfo(pycurl.HEADER_SIZE)
-        m['request-size'] = self.handle.getinfo(pycurl.REQUEST_SIZE)
-        m['content-length-download'] = self.handle.getinfo(pycurl.CONTENT_LENGTH_DOWNLOAD)
-        m['content-length-upload'] = self.handle.getinfo(pycurl.CONTENT_LENGTH_UPLOAD)
-        m['content-type'] = self.handle.getinfo(pycurl.CONTENT_TYPE)
-        m['response-code'] = self.handle.getinfo(pycurl.RESPONSE_CODE)
-        m['speed-download'] = self.handle.getinfo(pycurl.SPEED_DOWNLOAD)
-        m['ssl-verifyresult'] = self.handle.getinfo(pycurl.SSL_VERIFYRESULT)
+        keys = ["effective-url", "http-code", "total-time",
+                "namelookup-time", "connect-time", "pretransfer-time",
+                "redirect-time", "redirect-count", "size-upload",
+                "size-download", "speed-upload", "header-size", "request-size"
+                "content-length-download", "content-length-upload",
+                "content-type", "response-code", "speed-download"
+                "ssl-verifyresult", "filetime", "starttransfer-time",
+                "starttransfer-time", "redirect-time", "redirect-count",
+                "http-connectcode", "httpauth-avail", "proxyauth-avail",
+                "os-errno", "num-connects", "ssl-engines", "ftp-entry-path"
+                ]
+        for k in keys:
+            v = getattr(pycurl, k.replace("-", "_").upper())
+            m[k] = self.handle.getinfo(v)
+
         m['filetime'] = self.handle.getinfo(pycurl.INFO_FILETIME)
-        m['starttransfer-time'] = self.handle.getinfo(pycurl.STARTTRANSFER_TIME)
-        m['redirect-time'] = self.handle.getinfo(pycurl.REDIRECT_TIME)
-        m['redirect-count'] = self.handle.getinfo(pycurl.REDIRECT_COUNT)
-        m['http-connectcode'] = self.handle.getinfo(pycurl.HTTP_CONNECTCODE)
-        m['httpauth-avail'] = self.handle.getinfo(pycurl.HTTPAUTH_AVAIL)
-        m['proxyauth-avail'] = self.handle.getinfo(pycurl.PROXYAUTH_AVAIL)
-        m['os-errno'] = self.handle.getinfo(pycurl.OS_ERRNO)
-        m['num-connects'] = self.handle.getinfo(pycurl.NUM_CONNECTS)
-        m['ssl-engines'] = self.handle.getinfo(pycurl.SSL_ENGINES)
         m['cookielist'] = self.handle.getinfo(pycurl.INFO_COOKIELIST)
-        m['lastsocket'] = self.handle.getinfo(pycurl.LASTSOCKET)
-        m['ftp-entry-path'] = self.handle.getinfo(pycurl.FTP_ENTRY_PATH)
+
         return m
 
     def answered(self, check):
